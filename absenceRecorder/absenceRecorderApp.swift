0940ca4969ec10356e53ca9e8e94f93d7665cea6
Jamie <jxxthomson@gmail.com>
2022-02-03 18:32:45 +0000

    Initial Commit

---
+++ absenceRecorder/absenceRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  absenceRecorderApp.swift
+//  absenceRecorder
+//
+//  Created by Jamie Thomson on 03/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct absenceRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ absenceRecorderUITests/absenceRecorderUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  absenceRecorderUITestsLaunchTests.swift
+//  absenceRecorderUITests
+//
+//  Created by Jamie Thomson on 03/02/2022.
+//
+
+import XCTest
+
+class absenceRecorderUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
