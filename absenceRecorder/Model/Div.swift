cc5eb406cfd012b34c721dfd14afcad23996eac0
Jamie <jxxthomson@gmail.com>
2022-02-03 19:17:25 +0000

    halfway through classes

--- absenceRecorder/Model/Div.swift
+++ absenceRecorder/Model/Div.swift
@@ -15,4 +15,19 @@ class Div {
     init(code:String){
         self.code = code
     }
+    #if DEBUG
+    static func createDivision(code:String, of size: Int) -> Div{
+        let div = Div(code:code)
+        
+        //loop as many times as the param size says to create students and add thme to the students property
+        
+        return div
+        
+    }
+    static let examples = [Div.createDivision(code: "vBY-1", of: 1),
+                           Div.createDivision(code: "vCX-1", of: 12),
+                           Div.createDivision(code: "vEY-1", of: 8),
+                           Div.createDivision(code: "mDY-1", of: 96),
+                           Div.createDivision(code: "vFW-1", of: 99)]
+    #endif
 }
